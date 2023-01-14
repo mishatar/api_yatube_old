3c0070de14485951a85c9a5bac2413c5f90c219f
Mikhail <mishatar2004@gmail.com>
2023-01-14 19:30:02 +0300

    project_sprint8

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from posts.models import Comment, Group, Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+    group = serializers.SlugRelatedField(read_only=True, slug_field='title')
+
+    class Meta:
+        model = Post
+        fields = ('__all__')
